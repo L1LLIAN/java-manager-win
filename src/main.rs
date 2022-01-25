fe7c494b81f7b2d120bb184ced24856c4b5171f3
Lillian Armes <lily@hotgirl.zone>
2022-01-25 04:58:43 -0500

    wip: Add current source to not lose progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+extern crate winapi;
+
+use std::{ffi::CString, ptr::null_mut, str::from_utf8_mut};
+use winapi::{
+    shared::minwindef::HKEY,
+    um::{
+        winnt::{KEY_ALL_ACCESS, REG_SZ},
+        winreg::*,
+    },
+};
+
+fn get_path_from_reg() -> Vec<u8> {
+    unsafe {
+        let mut hkey: HKEY = null_mut();
+        let environment_str = CString::new("Environment").unwrap();
+
+        let open_status = RegOpenKeyExA(
+            HKEY_CURRENT_USER,
+            environment_str.as_ptr(),
+            0,
+            KEY_ALL_ACCESS,
+            &mut hkey,
+        );
+
+        // 0 = pog
+        // 87 = access denied
+        // 2 = no exist
+        if open_status != 0 {
+            RegCloseKey(hkey);
+            panic!("open_status non-zero");
+        }
+
+        let mut size: u32 = 6969;
+        let mut path_vec: Vec<u8> = vec![0; size as usize];
+        let mut reg_value_type = REG_SZ;
+
+        let path_str = CString::new("Path").unwrap();
+        let get_status = RegGetValueA(
+            HKEY_CURRENT_USER,
+            environment_str.as_ptr(),
+            path_str.as_ptr(),
+            RRF_RT_REG_SZ,
+            &mut reg_value_type,
+            path_vec.as_ptr() as *const _ as *mut _,
+            &mut size,
+        );
+
+        if get_status != 0 {
+            RegCloseKey(hkey);
+            panic!("get_status non zero");
+        }
+
+        RegCloseKey(hkey);
+
+        path_vec.resize(size as usize, 0);
+        return path_vec;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut path_vec = get_path_from_reg();
+    let mut path_str = from_utf8_mut(&mut path_vec).unwrap();
+    println!("path_str = {}", path_str);
 }
